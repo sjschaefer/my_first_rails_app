1c2ef5e3342675f45402ee3c30f73c3c1b22bcbc
sjschaefer <sjschaefer94@gmail.com>
2018-10-14 10:48:23 -0700

    initial commit

---
+++ db/migrate/20181014174044_create_cards.rb
@@ -0,0 +1,11 @@
+class CreateCards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cards do |t|
+      t.string :make
+      t.string :model
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/system/cards_test.rb
@@ -0,0 +1,47 @@
+require "application_system_test_case"
+
+class CardsTest < ApplicationSystemTestCase
+  setup do
+    @card = cards(:one)
+  end
+
+  test "visiting the index" do
+    visit cards_url
+    assert_selector "h1", text: "Cards"
+  end
+
+  test "creating a Card" do
+    visit cards_url
+    click_on "New Card"
+
+    fill_in "Make", with: @card.make
+    fill_in "Model", with: @card.model
+    fill_in "Year", with: @card.year
+    click_on "Create Card"
+
+    assert_text "Card was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Card" do
+    visit cards_url
+    click_on "Edit", match: :first
+
+    fill_in "Make", with: @card.make
+    fill_in "Model", with: @card.model
+    fill_in "Year", with: @card.year
+    click_on "Update Card"
+
+    assert_text "Card was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Card" do
+    visit cards_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Card was successfully destroyed"
+  end
+end
